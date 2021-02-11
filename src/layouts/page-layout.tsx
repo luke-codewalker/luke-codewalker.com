685b0426ec6c18177e8cf8231f31f6264e2247de
Luke Codewalker <arathorn089@arcor.de>
2021-02-12 00:38:12 +0100

    save dark theme to local storage

--- src/layouts/page-layout.tsx
+++ src/layouts/page-layout.tsx
@@ -5,6 +5,10 @@ interface PageLayoutProps {
     title: string;
 }
 
+enum Settings {
+    DARK_THEME_ON = 'darkThemeOn'
+}
+
 const PageLayout: FC<PageLayoutProps> = ({ children, title }) => {
     const [darkThemeActive, setDarkThemeActive] = useState<boolean>(true);
 
@@ -12,16 +16,24 @@ const PageLayout: FC<PageLayoutProps> = ({ children, title }) => {
         if (typeof window === 'undefined') {
             return;
         }
-
-        setDarkThemeActive(window.matchMedia('(prefers-color-scheme: dark)').matches);
+        const savedPreference = JSON.parse(window.localStorage.getItem(Settings.DARK_THEME_ON));
+        setDarkThemeActive(savedPreference ?? window.matchMedia('(prefers-color-scheme: dark)').matches);
     }, []);
+
+    const toggleThemePreference = () => {
+        setDarkThemeActive(!darkThemeActive);
+
+        if (typeof window !== 'undefined') {
+            window.localStorage.setItem(Settings.DARK_THEME_ON, JSON.stringify(!darkThemeActive));
+        }
+    }
 
 
 
     return <div className={`page ${darkThemeActive ? 'dark' : ''}`}>
         <title>{`${title} | Luke Codewalker`}</title>
         <label className="theme-toggle" htmlFor="dark-theme" title={`Switch to ${darkThemeActive ? 'light' : 'dark'} theme`}>{darkThemeActive ? '🌞' : '🌛'}
-            <input type="checkbox" name="dark-theme" id="dark-theme" checked={darkThemeActive} onChange={() => setDarkThemeActive(!darkThemeActive)} />
+            <input type="checkbox" name="dark-theme" id="dark-theme" checked={darkThemeActive} onChange={toggleThemePreference} />
         </label>
         {children}
     </div>
