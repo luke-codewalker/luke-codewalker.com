04833476661d6402b7150a53d6ada1fafdcf4464
Luke Codewalker <arathorn089@arcor.de>
2021-02-11 22:42:06 +0100

    render links as target blank and styling

--- src/components/contact-links/contact-links.tsx
+++ src/components/contact-links/contact-links.tsx
@@ -1,20 +1,21 @@
 import { graphql, useStaticQuery } from "gatsby";
 import React, { FC } from "react"
 import { ContentfulRichTextGatsbyReference, renderRichText, RenderRichTextData } from "gatsby-source-contentful/rich-text";
+import { INLINES } from '@contentful/rich-text-types';
 import "./contact-list.scss";
 
 type IconLinkData = {
-    allContentfulIconLink: {
-        nodes: {
-            iconName: string
-            linkText: RenderRichTextData<ContentfulRichTextGatsbyReference>;
-            order: number;
-        }[]
-    }
+  allContentfulIconLink: {
+    nodes: {
+      iconName: string
+      linkText: RenderRichTextData<ContentfulRichTextGatsbyReference>;
+      order: number;
+    }[]
+  }
 }
 
 const ContactLinks: FC = () => {
-    const data = useStaticQuery<IconLinkData>(graphql`
+  const data = useStaticQuery<IconLinkData>(graphql`
     query IconLinkQuery {
       allContentfulIconLink {
         nodes {
@@ -28,20 +29,24 @@ const ContactLinks: FC = () => {
     }
     `)
 
-    const { nodes: links } = data.allContentfulIconLink;
+  const { nodes: links } = data.allContentfulIconLink;
 
-    return (
-        <ul className="contact-list">
-            {
-                links
-                    .sort((a, b) => a.order - b.order)
-                    .map(
-                        (link, i) =>
-                            <li key={i}><i className={`icon-${link.iconName}`}></i>{renderRichText(link.linkText)}</li>
-                    )
-            }
-        </ul>
-    )
+  return (
+    <ul className="contact-list">
+      {
+        links
+          .sort((a, b) => a.order - b.order)
+          .map(
+            (link, i) =>
+              <li key={i}><i className={`icon-${link.iconName}`}></i>{renderRichText(link.linkText, {
+                renderNode: {
+                  [INLINES.HYPERLINK]: (node, children) => <a href={node.data.uri} target="_blank">{children}</a>
+                }
+              })}</li>
+          )
+      }
+    </ul>
+  )
 }
 
 export default ContactLinks;
