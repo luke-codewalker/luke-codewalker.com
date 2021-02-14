4df85e80dffedfdf2cda86a9268932ec4269ac0c
Luke Codewalker <arathorn089@arcor.de>
2021-02-14 16:10:37 +0100

    add console greetings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,15 @@ import * as React from "react"
 import PageLayout from "../layouts/page-layout";
 import "./index.scss";
 import { PageProps } from "gatsby";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import Hero from "../components/hero/hero";
 import Bio from "../components/bio/bio";
 import TechStack from "../components/tech-stack/tech-stack";
+import { consoleGreetings } from "../utils/console-greetings";
 
 const IndexPage: FC<PageProps> = () => {
+  useEffect(() => consoleGreetings(), []);
+
   return (
     <PageLayout title="Home">
       <main>
