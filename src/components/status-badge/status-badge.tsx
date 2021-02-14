74926d33590c9b1cc12ef7f6a2dbebe9896b71ef
Luke Codewalker <arathorn089@arcor.de>
2021-02-14 19:05:44 +0100

    refine styling and structure

--- src/components/status-badge/status-badge.tsx
+++ src/components/status-badge/status-badge.tsx
@@ -34,7 +34,7 @@ const StatusBadge: FC = () => {
 
     const { statusText, location } = data.allContentfulStatus.nodes.find(node => node.node_locale === currentLocale);
     return (
-        <span className="status-badge">{statusText}<a className="location" href={`https://www.google.com/maps/search/?api=1&query=${location.lat},${location.lon}`} target="_blank" rel="noreferrer, noopener"><i className="icon icon-location"></i></a></span>
+        <a className="status-badge" href={`https://www.google.com/maps/search/?api=1&query=${location.lat},${location.lon}`} target="_blank" rel="noreferrer, noopener">{statusText}<i className="icon icon-location"></i></a>
     )
 }
 
